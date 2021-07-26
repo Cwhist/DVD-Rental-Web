2a555a9ce41042f9fd5318c87f84b454990fc7ab
Cwhist <huichun97@naver.com>
2021-07-26 12:45:07 +0900

    Add Application / Application Test class

---
+++ src/main/java/com/example/dvdrental/DvdrentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.dvdrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dvdrental/DvdrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dvdrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
