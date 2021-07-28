f9eaf214ad7c5eac2ab8f3e8bc7d1b5dc4e69700
Cwhist <huichun97@naver.com>
2021-07-28 16:39:34 +0900

    Add Home page

---
+++ src/main/java/com/example/dvdrental/api/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.dvdrental.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(path = "/home")
+    public String home(Model model) {
+
+        model.addAttribute("intro", "DVD Rental");
+
+        return "home";
+    }
+
+}
